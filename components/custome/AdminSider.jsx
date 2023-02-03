701aa1403358974b29d15b8c0726e2c7b720dc3e
faroukaf <43047208+faroukaf@users.noreply.github.com>
2023-02-03 15:06:56 +0000

    6

--- components/custome/AdminSider.jsx
+++ components/custome/AdminSider.jsx
@@ -10,6 +10,9 @@ const Side = () => {
     
       <ListLink asvg="../svgs/profile.svg" title={'Profile'} subtitiles = {['profile details', '']} />
       <ListLink asvg="../svgs/teacher.svg" title={'Courses'} subtitiles = {[['Course 1', ''], ['Course 2', '']]} />
+      <ListLink asvg="../svgs/teacher.svg" title={'Program'} subtitiles = {[['PRogram 1', ''], ['Program 2', '']]} />
+      <Titile asvg="../svgs/plus.svg" title={'Add Program'} url="" />
+      <Titile asvg="../svgs/plus.svg" title={'Add User'} url="" />
 
       <Logout />
     </div>
