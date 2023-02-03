f02d4dfef0ee3354b0b06ba00135d4d2e049f5bd
faroukaf <43047208+faroukaf@users.noreply.github.com>
2023-02-03 16:31:10 +0000

    22

--- components/row/Logout.jsx
+++ components/row/Logout.jsx
@@ -1,7 +1,9 @@
-const Titile= ({ title, asvg }) => {
-    return (
+const Titile= () => {
+  title = "Logout";
+  
+  return (
       <div className = 'group flex text-xl m-2 align-middle hover:text-green-400'>
-          <img src="../svgs" alt="Your SVG" className='w-8 mr-4' />
+          <img src="../svgs/logout.svg" alt="Your SVG" className='w-8 mr-4' />
         {title}
       </div>
     )
