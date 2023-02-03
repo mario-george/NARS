74c0b48664c650fa6d927fca1b12e408df16c2e8
faroukaf <43047208+faroukaf@users.noreply.github.com>
2023-02-03 12:21:05 +0000

    1

--- components/Titile.jsx
+++ components/Titile.jsx
@@ -1,6 +1,6 @@
 const Titile= ({ title, asvg }) => {
     return (
-      <div className = 'group flex flex-col text-xl m-2 align-middle hover:text-green-400'>
+      <div className = 'group flex text-xl m-2 align-middle hover:text-green-400'>
           <img src={asvg} alt="Your SVG" className='w-8 mr-4' />
         {title}
       </div>
