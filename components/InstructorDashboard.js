50e8b3c29fd8f77c42723d179d140cf1fc7b6049
AhmedAchraf1 <96139974+AhmedAchraf1@users.noreply.github.com>
2023-02-15 20:54:55 +0200

    Add files via upload

--- components/InstructorDashboard.js
+++ components/InstructorDashboard.js
@@ -1,22 +1,56 @@
-import Link from 'next/link';
-import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
-import { header } from './header';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { header } from "./header";
+import { userActions } from "./store/userSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+import Cookies from "js-cookie";
 export default function InstructorDashboard() {
     const router = useRouter();
     const courseName = useSelector((s) => s.user.data.courses);
+    const dispatch = useDispatch();
+    const logoutHandler = () => {
+        dispatch(userActions.logOut());
+        window.location.href = "/logout";
+    };
     return (
         <nav className="nav2">
-            <Link className="link2 focus:text-green-400 " href="/instructor/profile">
-                Profile
-            </Link>
+            {header("Profile", [
+                <a
+                    className={
+                        router.pathname === "/instructor/profile"
+                            ? "activeLinkDashboard2"
+                            : "normalLinkDashboard2"
+                    }
+                    href="/instructor/profile"
+                >
+                    Profile details
+                </a>,
+            ])}
             {header('courses', [header(courseName,
                 [
-                    'course specs',
+                    <a
+                        className={
+                            router.pathname === "/instructor/coursespecs/part1"
+                                ? "activeLinkDashboard2"
+                                : "normalLinkDashboard2"
+                        }
+                        href="/instructor/coursespecs/part1"
+                    >
+                        Course Specs
+                    </a>,
                     'Materials',
                     'Assignments',
                     'Exams',
-                    'Grades',
+                    <a
+                        className={
+                            router.pathname === "/instructor/grades"
+                                ? "activeLinkDashboard2"
+                                : "normalLinkDashboard2"
+                        }
+                        href="/instructor/grades"
+                    >
+                    Grades
+                    </a>,
                     'Direct assesment',
                     'Indirect assesment'
                 ])
@@ -25,9 +59,12 @@ export default function InstructorDashboard() {
             <Link className="link2 focus:text-green-400 " href="/instructor/report">
                 Course report
             </Link>
-            <Link className="link2 focus:text-green-400 " href="/login">
+            <button
+                className="link2 focus:text-green-400 text-left mx-2"
+                onClick={logoutHandler}
+            >
                 Logout
-            </Link>
+            </button>
         </nav>
     );
 }
