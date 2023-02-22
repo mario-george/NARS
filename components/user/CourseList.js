12c28b79bf0f042c893b4035016736f23367ea19
AhmedAchraf1 <96139974+AhmedAchraf1@users.noreply.github.com>
2023-02-23 00:17:15 +0200

    Add files via upload

---
+++ components/user/CourseList.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function CourseList({ courses }) {
+    console.log(courses);
+
+    return (
+        <div className="container mx-auto my-4">
+            <table className="w-full text-left border rounded">
+                <thead className="bg-gray-100">
+                    <tr>
+                        <th className="px-4 py-2 font-bold border">Name</th>
+                        <th className="px-4 py-2 font-bold border">Code</th>
+                        <th className="px-4 py-2 font-bold border">Academic Year</th>
+                        <th className="px-4 py-2 font-bold border">FullMark</th>
+                        <th className="px-4 py-2 font-bold border">Faculty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courses.map((course) => {
+                        return (
+                            <tr key={course.email}>
+                                <td className="border px-4 py-2">{course.name}</td>
+                                <td className="border px-4 py-2">{course.code}</td>
+                                <td className="border px-4 py-2">{course.academicYear}</td>
+                                <td className="border px-4 py-2">{course.fullMark}</td>
+                                <td className="border px-4 py-2">{course.faculty}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+
+export default CourseList;
