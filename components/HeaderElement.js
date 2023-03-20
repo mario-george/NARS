119de6220f8cf17245ac9412d7fd86c3dd8500ba
moahemdsalama118 <mohamedsalama118@gmail.com>
2023-03-20 20:11:10 +0200

    fix infinity loop in courses

--- components/HeaderElement.js
+++ components/HeaderElement.js
@@ -1,44 +1,18 @@
 import { header2 } from "./header2";
-import Cookies from "js-cookie";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
-import { userActions } from "../components/store/userSlice";
 import { updateField } from "../components/store/userSlice";
 
-const { useState } = require("react");
-function HeaderElement({ id, name, createdAt, cookies }) {
+function HeaderElement({ id, originalId, name, createdAt }) {
   const dispatch = useDispatch();
-  // Call your Hook here
-  async function get_id(e, cookies) {
-    try {
-      const resp = await fetch(
-        `${process.env.url}api/v1/courses/created-courses/${e}`,
-        {
-          headers: {
-            Authorization: "Bearer " + cookies.token,
-          },
-        }
-      );
 
-      const dt = await resp.json();
-      Cookies.set("original_id", dt.data.course._id);
-      dispatch(
-        updateField({ field: "instance_id", value: dt.data.course._id })
-      );
-      //console.log(dt.data.course);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  const handel_set_cookies = (e, cookies) => {
-    get_id(e, cookies);
-    Cookies.set("instance_id", e);
-    dispatch(updateField({ field: "instance_id", value: e }));
-  };
   return header2(
     <Link
       href={`/instructor/courses/${id}/courseSpecs/part1`}
-      onClick={handel_set_cookies(id, cookies)}
+      onClick={() => {
+        dispatch(updateField({ field: "instance_id", value: id }));
+        dispatch(updateField({ field: "original_id", value: originalId }));
+      }}
     >
       {name}
       <br></br>
--- components/InstructorDashboard.js
+++ components/InstructorDashboard.js
@@ -11,11 +11,10 @@ import { CgLogOut } from "react-icons/cg";
 import { BsBook } from "react-icons/bs";
 import { GrAddCircle } from "react-icons/gr";
 
-export default function InstructorDashboard() {
+export default function InstructorDashboard({ cookies }) {
   const [c, sC] = useState([]);
   const coursesRef = useRef([]);
   const r = useRouter();
-  const cookies = useSelector((s) => s.cookies);
   const userState = useSelector((s) => s.user);
   const dispatch = useDispatch();
   const logoutHandler = () => {
@@ -27,39 +26,14 @@ export default function InstructorDashboard() {
     Cookies.set("instance_id", e);
     //window.location.href=`/instructor/courses/${e}/courseSpecs/part1`
   };
+
   useEffect(() => {
-    let newData33 = [];
+    console.log("Render");
+  });
+
+  useEffect(() => {
     console.log("TOKEN IS ", JSON.stringify(userState._id));
     console.log("TOKEN IS ", JSON.stringify(userState.token));
-
-    async function getCreatedCoursesForInstructor() {
-      const data = await fetch(
-        `${process.env.url}api/v1/courses/created-courses?instructor=${userState._id}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            Authorization: "Bearer " + userState.token,
-          },
-        }
-      );
-
-      const resp = await data.json();
-
-      sC(
-        resp.data.map((courseInstance) => {
-          const dateString = courseInstance.createdAt;
-          console.log("DATE IS ", JSON.stringify(courseInstance.course));
-          const dateOnly = dateString.split("T")[0];
-          return {
-            name: courseInstance.course.name,
-            createdAt: dateOnly,
-            _id: courseInstance._id,
-          };
-        })
-      );
-    }
 
     try {
       getCreatedCoursesForInstructor();
@@ -67,6 +41,25 @@ export default function InstructorDashboard() {
       console.log(e);
     }
   }, []);
+
+  async function getCreatedCoursesForInstructor() {
+    const data = await fetch(
+      `${process.env.url}api/v1/courses/created-courses?instructor=${userState._id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Bearer " + userState.token,
+        },
+      }
+    );
+
+    const resp = await data.json();
+
+    sC(resp.data);
+  }
+
   return (
     <nav className="nav44 scrollbar-none">
       <Link className="link2 focus:text-green-400 " href="/profile">
@@ -92,15 +85,16 @@ export default function InstructorDashboard() {
         </span>,
         [
           Array(
-            c.map((e) => {
+            c.map((courseInstance) => {
               return (
-                <div key={e._id} className=" mb-5 -mx-4  px-0 ">
+                <div key={courseInstance._id} className=" mb-5 -mx-4  px-0 ">
                   <HeaderElement
                     className={``}
-                    key={e._id}
-                    id={e._id.toString()}
-                    name={e.name}
-                    createdAt={e.createdAt}
+                    key={courseInstance._id}
+                    id={courseInstance._id.toString()}
+                    originalId={courseInstance.course._id.toString()}
+                    name={courseInstance.course.name}
+                    createdAt={courseInstance.createdAt.split("T")[0]}
                     cookies={cookies}
                   />
                 </div>
@@ -109,18 +103,6 @@ export default function InstructorDashboard() {
           ),
         ]
       )}
-
-      {/* {c.length!=0?c.map((e) => {
-         
-        
-         return header(e._id.toString(), [ "course specs", "Materials",
-          "Assignments", "Exams", "Grades", "Direct assesment",
-           "Indirect assesment", ]);
-
-          
-   
-        
-      }):null} */}
 
       <Link
         className="link2 focus:text-green-400 "
