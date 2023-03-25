fb4b0e798917c48ae44e274302740ced9bae9654
faroukaf <faroukafmsm1@gmail.com>
2023-03-25 22:12:12 +0200

    main report

--- pages/instructor/courses/[courseID]/report/courseReport.js
+++ pages/instructor/courses/[courseID]/report/courseReport.js
@@ -13,30 +13,31 @@ import CompetenciesAttainment from '@/components/chart/CompetenciesAttainment';
 import CompetenciesStatisticsTable from '@/components/chart/CompetenciesStatisticsTable';
 import CompetenciesBar from '@/components/chart/CompetenciesBar';
 import CLOBar from '@/components/chart/CLOBar';
-import CLOQ from '@/components/chart/CLOQ';
 import CLOAttainment from '@/components/chart/CLOAttainment';
 import CLOStatisticsTable from '@/components/chart/CLOStatisticsTable';
 import CLOBoxPlot from '@/components/chart/CLOBoxPlot';
 import CLOTable from '@/components/chart/CLOTable';
 import GradHist from "@/components/chart/GradHist";
 import getData from "@/components/chart/getData";
-import {
-  Chart as ChartJS,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-
-ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 const courseReport = ({ cookies }) => {
   const router = useRouter();
-  const [competence, setCompetences] = useState([]);
-  const [avgValues, setAvgValues] = useState([]);
+  const [competenciesMap, setCompetenciesMap] = useState({});
+  const [avgValues, setAvgValues] = useState({});
+  const [qs, setQs] = useState({});
+  const [quiz, setQuiz] = useState([]);
+  const [mid, setMid] = useState([]);
+  const [final, setFinal] = useState([]);
+  const [sNum, setSNum] = useState(0);
   const { courseID } = router.query;
+
+  const getAvg = (avgs) => {
+    return avgs.map(elm => {
+      let out = {};
+      out[elm.code.toUpperCase()] = elm.avg;
+      return out;
+    });
+  }
 
   useEffect(() => {
     getCourse();
@@ -53,14 +54,14 @@ const courseReport = ({ cookies }) => {
         }
       );
       const jsonData = await resp.json();
-      const competencesData = jsonData.data.avgCompetences.map(
-        (comp) => comp.code
-      );
-      const avgValuesData = jsonData.data.avgCompetences.map(
-        (comp) => comp.avg
-      );
-      setCompetences(competencesData);
-      setAvgValues(avgValuesData);
+      const [competenciesMapData, qsData, exams] = getData();
+      //setSNum();
+      const [quizData, midData, finalData] = exams;
+      setCompetenciesMap(competenciesMapData);
+      setQs(qsData);
+      setQuiz(quizData);
+      setMid(midData);
+      setFinal(finalData);
     } catch (e) {}
   };
 
@@ -70,30 +71,93 @@ const courseReport = ({ cookies }) => {
         <div className="contentAddUser2 flex flex-col gap-10 overflow-auto">
           <Navbar cookies={cookies} />
           <div className="w-full h-full flex justify-center items-center">
-            <Graph competence={competence} avgValues={avgValues} />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-3">
+              <p className="grad-title">
+                <span className="md:col-span-2">
+                    Quiz Grad
+                </span>
+              </p>
+              <Grad2Litter data={quiz} w={100} h={100} grid={15} title="Quiz"/>
+              <GradHist data={quiz} snum={sNnum} w={500} h={100} grid={15}/>
+
+                <p className="grad-title">
+                  <span className="md:col-span-2">
+                    Midterm Grad
+                  </span>
+                </p>
+              <Grad2Litter data={mid} w={100} h={100} grid={15} title="Midterm"/>
+              <GradHist data={mid} snum={sNnum} w={500} h={100} grid={15}/>
+
+                <p className="grad-title">
+                  <span className="md:col-span-2">
+                    Final Grad
+                  </span>
+                </p>
+              <Grad2Litter data={final} w={100} h={100} grid={15} title="Final"/>
+              <GradHist data={final} snum={sNnum} w={500} h={100} grid={15}/>
+            </div>
+            {/* Competencies & LOs*/}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-3">
+              <p className="grad-asm">
+                <span className="md:col-span-3">
+                    Competencies Exam Assessment
+                </span>
+              </p>
+              <CompetenciesQ cmap={competenciesMap} w={60} h={60} />
+              <CompetenciesBar cmap={avgValues} snum={sNum} w={60} h={60} grid={10}/>
+              <CompetenciesBoxPlot qs={qs} cmap={competenciesMap} snum={sNum} w={60} h={60} />
+
+              <div className="md:col-span-3">
+                <CompetenciesStatisticsTable qs={qs} cmap={competenciesMap} snum={sNum} w={20} h={10} />
+              </div>
+
+                {/* <p className="grad-asm">
+                  <span className="md:col-span-3">
+                    Competencies Survey Assessment
+                  </span>
+                </p>
+                <CompetenciesQ cmap={competenciesMap} w={60} h={60} title="Survey" />
+                <CompetenciesBar cmap={avgValues} snum={sNum} w={60} h={60} grid={10} title="Survey" />
+                <CompetenciesBoxPlot qs={qs} cmap={competenciesMap} snum={sNum} w={60} h={60} title="Survey" />
+
+                <div className="md:col-span-3">
+                  <CompetenciesStatisticsTable qs={qs} cmap={competenciesMap} snum={sNum} w={20} h={10} title="Survey" />
+                </div> */}
+
+                <p className="grad-asm">
+                  <span className="md:col-span-3">
+                    LOs Exam Assessment
+                  </span>
+                </p>
+                <CLOQ cmap={competenciesMap} clomap={CLO} />
+                <CLOBar cmap={avgValues} snum={sNum} w={20} h={10} grid={10} clomap={CLO} />
+                <CLOBoxPlot qs={qs} cmap={competenciesMap} snum={sNum} w={20} h={10} clomap={CLO} />
+
+                <div className="md:col-span-3">
+                  <CLOStatisticsTable clomap={CLO} qs={qs} cmap={competenciesMap} snum={sNum} w={20} h={10} />
+                </div>
+                <CLOAttainment clomap={CLO} cmap={avgValues} snum={sNum} w={20} h={10} />
+
+                {/* <p className="grad-asm">
+                  <span className="md:col-span-3">
+                    LOs Survey Assessment
+                  </span>
+                </p>
+                <CLOQ cmap={competenciesMap} clomap={CLO} />
+                <CLOBar cmap={avgValues} snum={sNum} w={20} h={10} grid={10} clomap={CLO} />
+                <CLOBoxPlot qs={qs} cmap={competenciesMap} snum={sNum} w={20} h={10} clomap={CLO} />
+
+                <div className="md:col-span-3">
+                  <CLOStatisticsTable clomap={CLO} qs={qs} cmap={competenciesMap} snum={sNum} w={20} h={10} />
+                </div>
+                <CLOAttainment clomap={CLO} cmap={avgValues} snum={sNum} w={20} h={10} /> */}
+            </div>
+
+
+
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-const Graph = ({ competence, avgValues }) => {
-  const data = {
-    labels: competence,
-    datasets: [
-      {
-        data: avgValues,
-        backgroundColor: "aqua",
-        borderColor: "black",
-        borderWidth: 1,
-      },
-    ],
-  };
-  const options = {};
-  return (
-    <div className="flex w-2/3 h-2/3  ">
-      <Bar data={data} options={options}></Bar>
     </div>
   );
 };
