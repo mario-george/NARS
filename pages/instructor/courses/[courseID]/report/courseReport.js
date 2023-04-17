1810edbf728056b7396550675eb6092dbd58ec60
moahemdsalama118 <mohamedsalama118@gmail.com>
2023-04-17 15:52:22 +0200

    read target from api

--- pages/instructor/courses/[courseID]/report/courseReport.js
+++ pages/instructor/courses/[courseID]/report/courseReport.js
@@ -26,7 +26,7 @@ const courseReport = ({ cookies }) => {
   const [avgValuesLOs, setAvgValuesLOs] = useState({});
   const [learningOutcomes, setLearningOutcomes] = useState({});
   const [courseLearningOutcomes, setCourseLearningOutcomes] = useState({});
-  const [target, setTarget] = useState([])
+  const [target, setTarget] = useState([]);
   const [questionsGrades, setQuestionsGrades] = useState({});
   const [courseData, setCourseData] = useState({});
   const [questions, setQuestions] = useState([]);
@@ -34,7 +34,7 @@ const courseReport = ({ cookies }) => {
   const [final, setFinal] = useState([]);
   const [numberOfStudents, setNumberOfStudents] = useState(0);
   const [dataLoaded, setDataLoaded] = useState(false);
-  const [wantedData, setWantedData] = useState([])
+  const [wantedData, setWantedData] = useState([]);
   const [studentAssessments, setStudentAssessments] = useState([]);
   const [lectureTopics, setLectureTopics] = useState([]);
   const { courseID } = router.query;
@@ -98,14 +98,20 @@ const courseReport = ({ cookies }) => {
         getData(jsonData.data.report.questions);
       setCourseCompetences(jsonData.data.course.competences);
       setNumberOfStudents(numOfStudents);
-      tempIt.push(getAvg(jsonData.data.report.avgCompetences))
+      tempIt.push(getAvg(jsonData.data.report.avgCompetences));
       setAvgValues(getAvg(jsonData.data.report.avgCompetences));
-      tempIt.push(getAvg(jsonData.data.report.avgCompetencesInDirect))
+      tempIt.push(getAvg(jsonData.data.report.avgCompetencesInDirect));
       setAvgValuesSurvey(getAvg(jsonData.data.report.avgCompetencesInDirect));
-      tempIt.push(getAvgLOs(jsonData.data.report.avgLOSInDirect))
+      tempIt.push(getAvgLOs(jsonData.data.report.avgLOSInDirect));
       setAvgValuesLOs(getAvgLOs(jsonData.data.report.avgLOSInDirect));
-      tempIt.push([50, 70])
-      setTarget([50, 70]);
+      tempIt.push([
+        jsonData.data.course.target.min,
+        jsonData.data.course.target.max,
+      ]);
+      setTarget([
+        jsonData.data.course.target.min,
+        jsonData.data.course.target.max,
+      ]);
       const { final, midterm } = examGrades;
       setCompetenciesMap(competences);
       setQuestionsGrades(questionsGrades);
@@ -117,12 +123,17 @@ const courseReport = ({ cookies }) => {
       );
       setLectureTopics(jsonData.data.courseSpecs.lecturePlan.topics);
       setDataLoaded(true);
-      tempIt.push()
+      tempIt.push();
       let myTemp = [jsonData.data.report.questions];
-      if(Object.keys(tempIt[0]).length && !tempIt[4][0]){myTemp.push('Direct Assessment isn\'t given')}
-      if(!tempIt[3][0]){myTemp.push('Target isn\'t given')}
-      if(!(Object.keys(tempIt[1]).length) && !(Object.keys(tempIt[2]).length))
-        {myTemp.push('Indirect Assessment isn\'t given')}
+      if (Object.keys(tempIt[0]).length && !tempIt[4][0]) {
+        myTemp.push("Direct Assessment isn't given");
+      }
+      if (!tempIt[3][0]) {
+        myTemp.push("Target isn't given");
+      }
+      if (!Object.keys(tempIt[1]).length && !Object.keys(tempIt[2]).length) {
+        myTemp.push("Indirect Assessment isn't given");
+      }
       setWantedData(myTemp);
     } catch (e) {
       console.log("ERROR", e);
@@ -131,85 +142,91 @@ const courseReport = ({ cookies }) => {
 
   return (
     <>
-      {wantedData[0] ? <div className='items-center text-center'>
-      {wantedData.map(elm => <h1 className='text-red'>{elm}</h1>)}
-      </div> : <>
-      {dataLoaded && (
-        <div className="flex flex-row w-screen h-screen mt-2">
-          <div className="bg-sky-50 h-screen w-[80%] translate-x-[25%] flex flex-col justify-center items-center text-black ml-1 scrollbar-none">
-            <div className="contentAddUser2 flex flex-col gap-10 overflow-auto">
-              <CourseData createdCourse={courseData} />
-              <AssessmentMethodsTable
-                questions={questions}
-                competences={courseCompetences}
-                studentAssessments={studentAssessments}
-              />
-              <CompetencesTable
-                className="flex justify-center items-center m-20"
-                courseCompetences={courseCompetences}
-                learningOutcomes={courseLearningOutcomes}
-              />
-              <TopicsTable
-                lectureTopics={lectureTopics}
-                learningOutcomes={courseLearningOutcomes}
-                courseID={courseID}
-                token={cookies.token}
-              />
-              <div className="flex flex-col justify-center items-center">
-                <ExamGrades
-                  mid={mid}
-                  final={final}
-                  numberOfStudents={numberOfStudents}
-                />
-                <CompetencesLosAchievement
-                  target={target}
-                  competenciesMap={competenciesMap}
-                  avgValues={avgValues}
-                  numberOfStudents={numberOfStudents}
-                  learningOutcomes={learningOutcomes}
-                />
-                <CompetencesLosGrades
-                  numberOfStudents={numberOfStudents}
-                  avgValues={avgValues}
-                  competenciesMap={competenciesMap}
-                  learningOutcomes={learningOutcomes}
-                />
-                <CompetencesLosAchievementSurvey
-                  target={target}
-                  competenciesMap={competenciesMap}
-                  avgValues={avgValuesSurvey}
-                  avgLOS={avgValuesLOs}
-                  numberOfStudents={numberOfStudents}
-                  learningOutcomes={learningOutcomes}
-                />
-                <CompetencesLosSurvey
-                  numberOfStudents={numberOfStudents}
-                  avgLOS={avgValuesLOs}
-                  avgValues={avgValuesSurvey}
-                  learningOutcomes={learningOutcomes}
-                />
-                <CompetencesLosAchievementOVerall
-                  target={target}
-                  competenciesMap={competenciesMap}
-                  avgLOS={avgValuesLOs}
-                  avgValues={avgValues}
-                  avgValuesSurvey={avgValuesSurvey}
-                  numberOfStudents={numberOfStudents}
-                  learningOutcomes={learningOutcomes}
-                />
-                <CompetencesLosOverall
-                  numberOfStudents={numberOfStudents}
-                  avgLOS={avgValuesLOs}
-                  avgValues={avgValues}
-                  avgValuesSurvey={avgValuesSurvey}
-                  learningOutcomes={learningOutcomes}
-                />
+      {wantedData[0] ? (
+        <div className="items-center text-center">
+          {wantedData.map((elm) => (
+            <h1 className="text-red">{elm}</h1>
+          ))}
+        </div>
+      ) : (
+        <>
+          {dataLoaded && (
+            <div className="flex flex-row w-screen h-screen mt-2">
+              <div className="bg-sky-50 h-screen w-[80%] translate-x-[25%] flex flex-col justify-center items-center text-black ml-1 scrollbar-none">
+                <div className="contentAddUser2 flex flex-col gap-10 overflow-auto">
+                  <CourseData createdCourse={courseData} />
+                  <AssessmentMethodsTable
+                    questions={questions}
+                    competences={courseCompetences}
+                    studentAssessments={studentAssessments}
+                  />
+                  <CompetencesTable
+                    className="flex justify-center items-center m-20"
+                    courseCompetences={courseCompetences}
+                    learningOutcomes={courseLearningOutcomes}
+                  />
+                  <TopicsTable
+                    lectureTopics={lectureTopics}
+                    learningOutcomes={courseLearningOutcomes}
+                    courseID={courseID}
+                    token={cookies.token}
+                  />
+                  <div className="flex flex-col justify-center items-center">
+                    <ExamGrades
+                      mid={mid}
+                      final={final}
+                      numberOfStudents={numberOfStudents}
+                    />
+                    <CompetencesLosAchievement
+                      target={target}
+                      competenciesMap={competenciesMap}
+                      avgValues={avgValues}
+                      numberOfStudents={numberOfStudents}
+                      learningOutcomes={learningOutcomes}
+                    />
+                    <CompetencesLosGrades
+                      numberOfStudents={numberOfStudents}
+                      avgValues={avgValues}
+                      competenciesMap={competenciesMap}
+                      learningOutcomes={learningOutcomes}
+                    />
+                    <CompetencesLosAchievementSurvey
+                      target={target}
+                      competenciesMap={competenciesMap}
+                      avgValues={avgValuesSurvey}
+                      avgLOS={avgValuesLOs}
+                      numberOfStudents={numberOfStudents}
+                      learningOutcomes={learningOutcomes}
+                    />
+                    <CompetencesLosSurvey
+                      numberOfStudents={numberOfStudents}
+                      avgLOS={avgValuesLOs}
+                      avgValues={avgValuesSurvey}
+                      learningOutcomes={learningOutcomes}
+                    />
+                    <CompetencesLosAchievementOVerall
+                      target={target}
+                      competenciesMap={competenciesMap}
+                      avgLOS={avgValuesLOs}
+                      avgValues={avgValues}
+                      avgValuesSurvey={avgValuesSurvey}
+                      numberOfStudents={numberOfStudents}
+                      learningOutcomes={learningOutcomes}
+                    />
+                    <CompetencesLosOverall
+                      numberOfStudents={numberOfStudents}
+                      avgLOS={avgValuesLOs}
+                      avgValues={avgValues}
+                      avgValuesSurvey={avgValuesSurvey}
+                      learningOutcomes={learningOutcomes}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          )}
+        </>
       )}
-      </>}
     </>
   );
 };
