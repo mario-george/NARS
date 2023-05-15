a9e7fa93487308ab6e41698aa615bb26a70044d0
faroukaf <faroukafmsm1@gmail.com>
2023-05-15 20:08:44 +0200

    courses graph1

--- pages/report/components/Graph1.js
+++ pages/report/components/Graph1.js
@@ -4,8 +4,12 @@ import Attainment from "@/components/chart/Attainment";
 
 const CoursesCompetences = ({ cookies }) => {
   const [courses, setCourses] = useState([]);
-  const [comp, setComp] = useState([]);
+  const [comp, setComp] = useState({});
+  const [coursesAvg, setCourseAvg] = useState({});
+  const [numSpecs, setNumSpecs] = useState([0, 0]); // [fill, not]
+  const [numReport, setNumReport] = useState([0, 0]);
   const checkboxRefs = useRef([[]]);
+
 
   checkboxRefs.current = Array.from({ length: courses.length }, () =>
     Array.from({ length: comp.length }, () => false)
@@ -63,21 +67,47 @@ const CoursesCompetences = ({ cookies }) => {
         console.log(e);
       }
     };
-    get_comp();
-    get_courses();
+    // get_comp();
+    // get_courses();
+    setComp({
+      "A1" : {
+        "direct" : 40,
+        "indirect" : 60,
+        "avg" : 50
+      },
+      "B2" : {
+        "direct" : 70,
+        "indirect" : 80,
+        "avg" : 75
+      }
+    })
+    setCourses({
+      "Pro" : {
+        "direct" : 40,
+        "indirect" : 60,
+        "avg" : 50
+      },
+      "Lite" : {
+        "direct" : 70,
+        "indirect" : 80,
+        "avg" : 75
+      }
+    })
+    setNumReport([3, 2])
+    setNumSpecs([2, 3])
   }, []);
 
-  //to check the boxes for the given competences of courses
-  courses.map((course, i) => {
-    let comps = course.competences;
-    checkboxRefs.current[i].forEach((isChecked, index) => {
-      comps.map((e) => {
-        if (e.code === comp[index].code) {
-          checkboxRefs.current[i][index] = true;
-        }
-      });
-    });
-  });
+  // //to check the boxes for the given competences of courses
+  // courses.map((course, i) => {
+  //   let comps = course.competences;
+  //   checkboxRefs.current[i].forEach((isChecked, index) => {
+  //     comps.map((e) => {
+  //       if (e.code === comp[index].code) {
+  //         checkboxRefs.current[i][index] = true;
+  //       }
+  //     });
+  //   });
+  // });
 
   return (
     <>
@@ -88,34 +118,44 @@ const CoursesCompetences = ({ cookies }) => {
         </h2>
         <div className="h-0.5 w-full bg-gray-300 mb-2" />
         <div className="flex flex-col w-full items-center">
-          <label className="mt-12">Competences Achievement</label>
+        <label className="mt-12">Competences Achievement</label>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-2 gap-y-1 w-[90%]">
           <div>
             <Attainment
               target={target}
-              cAvg={avgAvg}
-              avgS={[
-                avgValues,
-                avgValuesSurvey,
-              ]}
+              cAvg={avgValues}
               w={20}
               h={20}
-              title={"All Assessment"}
+              title={"Survey"}
+              />
+          </div>
+          <div>
+            <AttainmentPie target={target} cAvg={avgValues} title={"Survey"} />
+          </div>
+        </div>
+        <label className="mt-12">Learning Outcomes Achievement</label>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-2 gap-y-1 w-[90%] mb-10">
+          <div>
+            <CLOAttainment
+              target={target}
+              clomap={learningOutcomes}
+              title={"Survey"}
+              w={20}
+              h={20}
+              avgLOS={avgLOS}
             />
           </div>
-          <label className="mt-12">Courses Achievement</label>
-          <Attainment
+          <div>
+            <CLOAttainmentPie
               target={target}
-              cAvg={avgAvg}
-              avgS={[
-                avgValues,
-                avgValuesSurvey,
-              ]}
-              w={20}
-              h={20}
-              title={"All Assessment"}
+              clomap={learningOutcomes}
+              title={"Survey"}
+              avgLOS={avgLOS}
             />
+          </div>
         </div>
       </div>
+    </div>
       )}
     </>
   );
